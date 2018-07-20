a7263462a9699f62796d7387d2d17b0e6931bbea
codecrook <hello@codecrook.io>
2018-07-21 00:14:07 +0530

    Fix: Fixed bug in the search functionality
    Bug Fixes:
    - Earlier the complete list of locations wasn't shown by default.
    - Fixed bug to show all the location in the list by default and filter according to search query
    
    - Earlier markers were not getting filtered according to search query
    - Fixed bug to show markers for the locations only matching to search query

--- js/app.js
+++ js/app.js
@@ -5,9 +5,11 @@
     var Bhubaneswar = {lat: 20.2961, lng: 85.8245};
     var openWeatherMapApiKey = '2d3be81cb9645506add717caa301f5d4';
 
-    var infowindow = new google.maps.InfoWindow();
+    var infowindow;
+    var bounds;
     
-    var weatherInfoContent, weatherIcon;
+    var weatherInfoContent;
+    var weatherIcon;
 
 
     function initMap() {
@@ -16,6 +18,9 @@
             zoom: 11,
             mapTypeControl: false
         });
+        
+        infowindow = new google.maps.InfoWindow();
+        bounds = new google.maps.LatLngBounds();
     }
     
     function googleMapError() {
@@ -36,6 +41,8 @@
         this.lng = lng;
         this.placeID = placeID;
         
+        
+        this.visible = ko.observable(true);
         //function to get details about a 'dhaba' and weather info of the city
         this.getData = function () {
             var rating;
@@ -57,7 +64,7 @@
                         phone_no = 'Not Found!';
                     }
                     
-                    $.each(result.reviews, function(i, review) {
+                    $.each(result.reviews, function (i, review) {
                         reviews.push(`<li> ${review.text} </li>`);
                     });
                     
@@ -78,34 +85,54 @@
             //Code to get Weather info of the city from OpenWeatherMap API
             var weatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${Bhubaneswar.lat}&lon=${Bhubaneswar.lng}&appid=${openWeatherMapApiKey}&units=metric`;
             
-            $.ajax({
-                url: weatherURL,
-                dataType: 'json'
-            }).done(function(weatherInfo){
-                weatherInfoContent =
-                    `<img class="weather-icon" src="http://openweathermap.org/img/w/${weatherInfo.weather[0].icon}.png">
-                     <h3 class="weather-description">${weatherInfo.weather[0].description}</h3>
-                     <p class="weather-temp"><i class="fas fa-thermometer-half"></i>  ${weatherInfo.main.temp} &#8451;</p>`;
-                
-                $('.weather-info').html(weatherInfoContent);
-            }).fail(function(){
-                weatherInfoContent = `<p class="error-msg'>Error in getting data from OpenWeatherMap</p>`;
-                $('.weather-info').html(weatherInfoContent);
-            });
+            //get live weather data in every 10 seconds
+            setInterval(function(){
+                $.ajax({
+                    url: weatherURL,
+                    dataType: 'json'
+                }).done(function(weatherInfo){
+                    weatherInfoContent =
+                        `<img class="weather-icon" src="http://openweathermap.org/img/w/${weatherInfo.weather[0].icon}.png">
+                         <h3 class="weather-description">${weatherInfo.weather[0].description}</h3>
+                         <p class="weather-temp"><i class="fas fa-thermometer-half"></i>  ${weatherInfo.main.temp} &#8451;</p>`;
+                    
+                    $('.weather-info').html(weatherInfoContent);
+                }).fail(function(){
+                    weatherInfoContent = `<p class="error-msg'>Error in getting data from OpenWeatherMap</p>`;
+                    $('.weather-info').html(weatherInfoContent);
+                });
+            }, 10000);
+            
         }();
         
+        //code to make the marker for the given location
         this.marker = new google.maps.Marker({
             position: {lat: self.lat, lng: self.lng},
             icon: createMarkerIcon('990000'),
             animation: google.maps.Animation.DROP,
-            map: map,
             title: self.title
         });
         
+        //function to show the markers on map according to their vivsibility value
+        this.filterMarkers = ko.computed(function() {
+            if (self.visible() === true) {
+                self.marker.setMap(map);
+                bounds.extend(self.marker.position);
+                map.fitBounds(bounds);
+            } else {
+                self.marker.setMap(null);
+            }
+        });
+        
+        //function to create the InfoWindow for an individual marker
         this.openInfowindow = function() {
             map.panTo(self.marker.getPosition());
             infowindow.setContent(self.content);
             infowindow.open(map,self.marker);
+            
+            infowindow.addListener('closeclick', function(){
+                infowindow.marker = null;
+            });
         };
         
         this.addListener = google.maps.event.addListener(self.marker,'click', (this.openInfowindow));
@@ -117,13 +144,14 @@
         this.marker.addListener('mouseout', function() {
             this.setIcon(createMarkerIcon('990000'));
         });
+        
     };
     
     var DhabaLocationModel = {
         locations: [
             new DhabaLocation('Jungle View', 20.401943, 85.806067, 'ChIJH62rupQOGToRFiMvN5pH5m4'),
             new DhabaLocation('Nakli Dhaba', 20.300328, 85.819313, 'ChIJ70Z_acYJGToRpoKdWV3ryk0'),
-            new DhabaLocation('Brother\'s Dahaba', 20.2169217, 85.8518417, 'ChIJO8A4iTOhGToRCB6ms6N6yCM'),
+            new DhabaLocation('Brother\'s Dhaba', 20.2169217, 85.8518417, 'ChIJO8A4iTOhGToRCB6ms6N6yCM'),
             new DhabaLocation('Yummyies Dhaba', 20.356689, 85.8887489, 'ChIJkaYOyZ8LGToRvDnL9952RUw'),
             new DhabaLocation('Behera Dhaba', 20.2337221, 85.74291549, 'ChIJHVC1QF-oGToROltP04gs5qQ'),
             new DhabaLocation('Jaalazza - The Urban Dhaba', 20.3556818, 85.81583980000005, 'ChIJiSQJAyMJGToRvzMdfjsB-mg'),
@@ -134,16 +162,22 @@
         searchQuery: ko.observable('')
     };
     
-    DhabaLocationModel.search = ko.dependentObservable(function() {
+    //function to search locations
+    DhabaLocationModel.search = ko.computed(function() {
         var self = this;
         var search = this.searchQuery().toLowerCase();
         return ko.utils.arrayFilter(self.locations, function(location) {
-            return location.title.toLowerCase().indexOf(search) >= 0;
+            var result = location.title.toLowerCase().indexOf(search) >= 0;
+            location.visible(result);
+            return result;
         });
+        
     }, DhabaLocationModel);
     
     ko.applyBindings(DhabaLocationModel);
     
+    
+    //function to create marker icon using Google Chart API
     function createMarkerIcon(markerColor) {
         var markerImage = new google.maps.MarkerImage(
             'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|' + markerColor + '|40|_|%E2%80%A2',
